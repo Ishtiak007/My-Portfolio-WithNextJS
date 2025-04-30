4664fd59f4a9f83dff577177818657888d0a14b3
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-05-01 00:45:52 +0600

    resolved a issue for the floating navbar now its look better

--- src/app/(WithCommonLayout)/layout.tsx
+++ src/app/(WithCommonLayout)/layout.tsx
@@ -1,9 +1,10 @@
+import { Navbar } from "@/components/shared/Navbar";
 import { ReactNode } from "react";
 
 export default function CommonLayout({ children }: { children: ReactNode }) {
   return (
     <div>
-      {/* <Navbar /> */}
+      <Navbar />
       <div className="min-h-screen">{children}</div>
       {/* <Footer /> */}
     </div>
