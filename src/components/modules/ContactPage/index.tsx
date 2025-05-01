39fbb75aa6f17a5adcf251d3fb3e4c91414880c5
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-05-02 02:58:14 +0600

    Contact related email send task completed

--- src/components/modules/ContactPage/index.tsx
+++ src/components/modules/ContactPage/index.tsx
@@ -2,27 +2,46 @@
 
 import { BackgroundBeams } from "@/components/ui/background-beams";
 import { Button } from "@/components/ui/moving-border";
-import React, { FormEvent, useState } from "react";
+import React, { FormEvent, useRef, useState } from "react";
+import Swal from "sweetalert2";
+import emailjs from "@emailjs/browser";
 
 function ContactMe() {
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const form = useRef<HTMLFormElement>(null);
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("Submitted:", { email, message });
+  const sendEmail = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!form.current) return;
+
+    emailjs
+      .sendForm(
+        "service_cawnu8j",
+        "template_f3kyxvg",
+        form.current,
+        "0EucXb_zUMFzsGLwF"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          if (result.text === "OK") {
+            Swal.fire(
+              "Good job!",
+              "Message has been sent successfully!",
+              "success"
+            );
+          }
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
 
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900 py-12 pt-36 relative">
-      {" "}
-      {/* Ensure the container is relative */}
-      {/* BackgroundBeams with adjusted z-index */}
       <BackgroundBeams className="absolute top-0 left-0 w-full h-full z-0" />
-      {/* Content with higher z-index */}
       <div className="max-w-2xl mx-auto p-4 relative z-10">
-        {" "}
-        {/* Add relative and z-10 to bring content to the front */}
         <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8 text-white">
           Contact With Me
         </h1>
@@ -32,20 +51,39 @@ function ContactMe() {
           together, or need help with web development, feel free to reach out.
           Let’s build something amazing together!
         </p>
-        <form onSubmit={handleSubmit} className="space-y-4 mt-4">
+        <form ref={form} onSubmit={sendEmail} className="space-y-4 mt-4">
           <input
             type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            name="from_email"
             placeholder="Your email address"
-            className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500 w-full p-4 bg-neutral-950 placeholder:text-gray-400 "
+            className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500 w-full p-4 bg-neutral-950 placeholder:text-gray-400"
+            required
+          />
+          <input
+            type="text"
+            name="from_name"
+            placeholder="Your Name"
+            className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500 w-full p-4 bg-neutral-950 placeholder:text-gray-400"
+            required
+          />
+          <input
+            type="text"
+            name="from_subject"
+            placeholder="Subject"
+            className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500 w-full p-4 bg-neutral-950 placeholder:text-gray-400"
+            required
+          />
+          <input
+            type="number"
+            name="phoneNumber"
+            placeholder="Number"
+            className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500 w-full p-4 bg-neutral-950 placeholder:text-gray-400"
             required
           />
           <textarea
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
+            name="message"
             placeholder="Your message"
-            className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500 w-full p-4 bg-neutral-950 placeholder:text-gray-400  resize-none"
+            className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500 w-full p-4 bg-neutral-950 placeholder:text-gray-400 resize-none"
             rows={5}
             required
           ></textarea>
